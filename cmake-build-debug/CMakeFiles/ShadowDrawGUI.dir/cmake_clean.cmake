25f16ac8aeffebe51180571f45b0082d80015ca8
Stephan <stephan.lorbek@aon.at>
2019-03-20 15:01:30 +0100

    added assignment description

---
+++ cmake-build-debug/CMakeFiles/ShadowDrawGUI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShadowDrawGUI.dir/main.cpp.o"
+  "ShadowDrawGUI.pdb"
+  "ShadowDrawGUI"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShadowDrawGUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
